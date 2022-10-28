47d79fb3c25cc09825bb3d9d3a36bd101fba26ae
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2022-10-27 21:39:30 -0300

    feat: ensure Home will not render Dialog on start

--- src/presentation/components/dialog/dialog.tsx
+++ src/presentation/components/dialog/dialog.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
-import Styles from './header-styles.scss'
+import Styles from './dialog-styles.scss'
 import CLOSE_ICON from '@/presentation/assets/icons/close.svg'
 
-const Dialog: React.FC = () => {
+type CloseType = () => void
+
+type Props = {
+  title?: string
+  isOpen: boolean
+  closeOnOverlayClick: boolean
+  children?: React.ReactNode
+  onClose: CloseType
+}
+
+const Dialog: React.FC<Props> = (props: Props) => {
+  const {
+    title,
+    isOpen,
+    closeOnOverlayClick,
+    onClose,
+    children
+  } = props
+
   return (
-    <div className={Styles.overlay}>
-      <div>
-        <img src={CLOSE_ICON} />
-        <span>Title</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ior sit amet, consectetur adipiscing elit, sed do eiusmod tempor idor sit amet, consectetur adipiscing elit, sed do eiusmod tempor iddor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor idolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit Lorem ipsum dolor sit amet,</p>
-      </div>
+    <div data-testid='dialog-wrap'>
+      {isOpen && (
+        <div className={Styles.dialog} onClick={() => closeOnOverlayClick && onClose()}>
+          <div className={Styles['inner-container']}>
+            <div className={Styles['title-container']}>
+              <span>{title}</span>
+              <img src={CLOSE_ICON} />
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './home-styles.scss'
 import { Dialog, Footer, Header } from '@/presentation/components'
 
 const Home: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <div className={Styles.home}>
       <Header />
@@ -10,7 +12,16 @@ const Home: React.FC = () => {
         <button>Open Dialog</button>
       </section>
       <Footer />
-      {false && <Dialog />}
+      <Dialog
+        title='Title'
+        isOpen={isOpen}
+        closeOnOverlayClick={true}
+        onClose={() => setIsOpen(false)}
+      >
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit Lorem ipsum dolor sit amet,
+        </p>
+      </Dialog>
     </div>
   )
 }
